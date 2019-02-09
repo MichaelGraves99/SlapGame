a59f4b399b7f81831174f7af8b56fd7d02d17864
Michael Graves <msgraves99@gmail.com>
2019-02-08 19:54:14 -0700

    Part 2 at step 6

--- app/game.js
+++ app/game.js
@@ -1,26 +1,82 @@
-let health = 100;
-let targetName = "dude";
-let hits = 0;
+let ken = {
+  health: 120,
+  hits: 0,
+  attacks: {
+    kick: 20,
+    punch: 15,
+    uppercut: 30,
+    hadouken: 40
+  },
+  mobility: 35,
+  items: []
+}
+
+let ryu = {
+  health: 100,
+  hits: 0,
+  attacks: {
+    kick: 15,
+    punch: 10,
+    uppercut: 25,
+    hadouken: 60
+  },
+  mobility: 55,
+  items: []
+}
+
+let items = {
+  fire: {
+    name: 'Fire',
+    modifier: 4,
+    description: 'IT BURNS!'
+  },
+  ice: {
+    name: 'Hammer',
+    modifier: 2,
+    description: 'Ouch!!'
+  },
+  thunder: {
+    name: 'Thunder',
+    modifier: 3,
+    description: 'Its so LOUD!!'
+  }
+}
+
+function giveItem() {
+  ryu.items.push(items.fire)
+
+}
+
+function addMods() {
+  let modTotal = 0;
+  for (let i = 0; i < ryu.items.length; i++) {
+    modTotal += ryu.items[i].modifier
+  }
+  return modTotal;
+}
+
+
+
 
 function slap() {
-  health--;
-  hits++
+  ryu.health -= (1 + addMods());
+  ryu.hits++
   update();
 }
 function punch() {
-  health -= 5;
-  hits++
+  ryu.health -= (5 + addMods());
+  ryu.hits++
   update();
 }
 function kick() {
-  health -= 10;
-  hits++
+  ryu.health -= (10 + addMods());
+  ryu.hits++
   update();
 }
 
 function update() {
-  document.getElementById('health').innerText = `${health}`
-  document.getElementById('hits').innerText = `${hits}`
+  document.getElementById('health').innerText = `${ryu.health}`
+  document.getElementById('hits').innerText = `${ryu.hits}`
 
 }
 
